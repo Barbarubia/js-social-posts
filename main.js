a68f185d655cc6d9566b4cfb2c326c9955a43a9e
Barbarubia <dnl.desimone@gmail.com>
2022-03-09 08:04:39 -0300

    ripristinato href=# utilizzo event.preventDefault

--- main.js
+++ main.js
@@ -149,7 +149,7 @@ function postGenerator(i) {
     elePostLikes.append(elePostLikesCta);
     let eleLikeButton = document.createElement('a');
     eleLikeButton.classList.add('like-button', 'js-like-button');
-    eleLikeButton.href = '#!';
+    eleLikeButton.href = '#';
     eleLikeButton.dataset.postid = posts[i].id;
     eleLikeButton.innerHTML = `<i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
     <span class="like-button__label">Mi Piace</span>`;
@@ -206,7 +206,10 @@ return arrItalianDateFormat;
 
 
 // Funzione per applicare e rimuovere il like
-function addRemoveLike() {
+function addRemoveLike(event) {
+// Evito che al click sul bottone la pagina torni all'inizio
+event.preventDefault();
+
 let likesCounter = parseInt(this.parentNode.parentNode.parentNode.querySelector('.js-likes-counter').textContent);
 const arrIDwithLike = [];
 
